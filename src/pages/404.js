278adf64fb8341daeacb93f6758bbf130f401707
Vinay Puppal <me@vinaypuppal.com>
2018-04-22 06:03:18 +0530

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Link from 'gatsby-link';
+import MdInfo from 'react-icons/lib/fa/bullhorn';
+
+import { Container, Content, PlaceHolder } from 'components/layout';
+import { H1 } from 'components/text';
+
+const title = 'Page Not Found | vinaypuppal.com';
+
+const NotFoundPage = () => (
+  <Container>
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:site_name" content={title} />
+      <meta name="twitter:title" content={title} />
+      <meta itemProp="name" content={title} />
+    </Helmet>
+    <Content>
+      <PlaceHolder>
+        <MdInfo size={100} />
+        <H1>404 | Page Not Found</H1>
+        <Link to="/">Go Back Home</Link>
+      </PlaceHolder>
+    </Content>
+  </Container>
+);
+
+export default NotFoundPage;
